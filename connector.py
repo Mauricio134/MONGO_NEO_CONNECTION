5a2b580c81aaf11a417334fc79daf10ab1e118b7
Mauricio Apaza <mauricioapaza@MacBook-Pro-de-Mauricio.local>
2023-06-20 18:42:06 -0500

    Trabajo Finalizado

--- connector.py
+++ connector.py
@@ -3,11 +3,10 @@ import json
 from neo4j import GraphDatabase
 from bson import ObjectId
 
-client = MongoClient("mongodb://localhost:27017/") # Conexion con MongoDB
-db = client["awards"] # Conexion a una base de datos
-collection = db["Oscars"] # Conexion con la colección
+client = MongoClient("mongodb://localhost:27017/")
+db = client["awards"]
+collection = db["Oscars"]
 
-# Configurar la conexión a Neo4j
 neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "mauricioapaza"))
 neo4j_session = neo4j_driver.session()
 
@@ -32,31 +31,33 @@ for item in result:
     winner = item["winner"]
     
     if first_item:
-         record = neo4j_session.run("CREATE (p:Principal {year_film: $year_film, year_ceremony: $year_ceremony, ceremony: $ceremony, category: $category, name: $name, film: $film, winner: $winner})",
+        record = neo4j_session.run("CREATE (p:Principal {year_film: $year_film, year_ceremony: $year_ceremony, ceremony: $ceremony, category: $category, nombre: $nombre, film: $film, winner: $winner, name: $display_name}) RETURN p",
                           year_film=year_film,
                           year_ceremony=year_ceremony,
                           ceremony=ceremony,
                           category=category,
-                          name=name,
+                          nombre=name,
                           film=film,
-                          winner=winner)
-         first_item = False
+                          winner=winner,
+                          display_name=film + ' - ' + name)  # Concatena film y name en display_name
+        first_item = False
     else:
-         record = neo4j_session.run("CREATE (p:Pelicula {year_film: $year_film, year_ceremony: $year_ceremony, ceremony: $ceremony, category: $category, name: $name, film: $film, winner: $winner})",
+        record = neo4j_session.run("CREATE (p:Pelicula {year_film: $year_film, year_ceremony: $year_ceremony, ceremony: $ceremony, category: $category, nombre: $nombre, film: $film, winner: $winner, name: $display_name}) RETURN p",
                           year_film=year_film,
                           year_ceremony=year_ceremony,
                           ceremony=ceremony,
                           category=category,
-                          name=name,
+                          nombre=name,
                           film=film,
-                          winner=winner)
-         # Crear relación con el primer nodo basado en year_film
-         cypher_query = "MATCH (a:Principal {year_film: $first_year_film}), (b:Pelicula {year_film: $current_year_film}) MERGE (a)-[:SE_ESTRENO_JUNTO_A]->(b)"
-         neo4j_session.run(cypher_query, first_year_film=year_film, current_year_film=1927)
+                          winner=winner,
+                          display_name=film + ' - ' + name)  # Concatena film y name en display_name
+        cypher_query = "MATCH (a:Principal {year_film: $first_year_film}), (b:Pelicula {year_film: $current_year_film}) MERGE (a)-[:SE_ESTRENO_JUNTO_A]->(b)"
+        neo4j_session.run(cypher_query, first_year_film=year_film, current_year_film=1927)
 
-a = input("Quieres Continuar: ")
-if a == 'y' :
-   cypher_query = "MATCH (n) DETACH DELETE n"
-   neo4j_session.run(cypher_query)
+a = input("¿Deseas continuar? ")
+if a == 'y':
+    cypher_query = "MATCH (n) DETACH DELETE n"
+    neo4j_session.run(cypher_query)
+
 client.close()
 neo4j_driver.close()
